d157ce859437b872b3baf4763454213330f2883c
lilylovecode123 <lilyzxycu@163.com>
2024-09-07 18:15:07 +0800

    Finished unit test in client_test.py file

--- client_test.py
+++ client_test.py
@@ -8,6 +8,15 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      stock, top_bid, top_ask, price = getDataPoint(quote)
+      assert stock == quote['stock']
+      assert top_bid == quote['top_bid']['price']
+      assert top_ask == quote['top_ask']['price']
+      assert price == (quote['top_bid']['price'] + quote['top_ask']['price']) / 2
+
+
+
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
